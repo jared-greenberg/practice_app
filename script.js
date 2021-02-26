d013d3485f228662c93ba3dcc4847755012a1abf
jared-greenberg <jared.p.greenberg@gmail.com>
2021-02-26 15:58:30 -0500

    Attach js script

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+   const email = document.getElementById("email");
+   const password = document.getElementById("password");
+   const login = document.getElementById("login");
+   const form = document.querySelector(".login-form");
+   
+   
+   
+   
+})
