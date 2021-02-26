59a143dedb639c39159d8a801210435bfe7e037c
jared-greenberg <jared.p.greenberg@gmail.com>
2021-02-26 17:00:33 -0500

    Add basic login form

--- practice-app/src/components/login_form.js
+++ practice-app/src/components/login_form.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-export default () => {
+const LoginForm = () => {
+
+
+
+
+
   return (
-    <h1>Hello</h1>
+    <form>
+      <div className="text-inputs">
+        <label> Email
+          <input type="text" placeholder="user@rapptrlabs.com"/>
+        </label>
+
+        <label> Password
+          <input type="password" placeholder="Must be at least 4 characters" />
+        </label>
+      </div>
+
+      <input type="submit" value="login"/>
+
+
+    </form>
   )
 }
+
+export default LoginForm;
+
--- practice-app/src/root.js
+++ practice-app/src/root.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'; 
 import { Route, Switch } from 'react-router-dom';
+import LoginForm from './components/login_form'
 
-export default ({store} => {
+export default ({store}) => (
   <Provider store={store}>
     <HashRouter>
 
       <Switch>
-        <Route path="/" component={}/>
-        <Route path="/" component={}/>
+        <Route path="/" component={LoginForm}/>
       </Switch>
 
-    </HashRouter>
+    </HashRouter> 
   </Provider>
-})
+)
