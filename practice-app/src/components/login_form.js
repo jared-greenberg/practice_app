8671bc794b6a1dafdb2d16542919a39241844cd3
jared-greenberg <jared.p.greenberg@gmail.com>
2021-02-27 15:13:08 -0500

    Create login form and styling

--- practice-app/src/components/login_form.js
+++ practice-app/src/components/login_form.js
@@ -1,24 +1,25 @@
-// import React, {useEffect, useState} from 'react';
+import React from 'react';
+import './login_form.css';
 
 const LoginForm = () => {
 
-
-
-
-
   return (
-    <form>
+    <form className="login-form">
+
+      <h1>Rapptr Labs</h1>
       <div className="text-inputs">
         <label> Email
           <input type="text" placeholder="user@rapptrlabs.com"/>
+          <i class="fas fa-user"></i>
         </label>
 
         <label> Password
-          <input type="password" placeholder="Must be at least 4 characters" />
+          <input type="password" placeholder="Must be at least 4 characters"/>
+          <i class="fas fa-lock"></i>
         </label>
       </div>
 
-      <input type="submit" value="login"/>
+      <input type="submit" value="Login"/>
 
 
     </form>
