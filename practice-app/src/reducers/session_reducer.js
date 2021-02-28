97475e28a2f17f39a3bbeb1e7015233df38a8329
jared-greenberg <jared.p.greenberg@gmail.com>
2021-02-28 11:38:43 -0500

    Add user to local storage

--- practice-app/src/reducers/session_reducer.js
+++ practice-app/src/reducers/session_reducer.js
@@ -3,8 +3,10 @@ import { RECEIVE_CURRENT_USER, LOGOUT_USER } from '../actions/session_actions';
 const sessionReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_CURRENT_USER: 
+      localStorage.setItem("appCurrentUser", JSON.stringify(action.currentUser));
       return action.currentUser
     case LOGOUT_USER:
+      localStorage.removeItem("appCurrentUser");
       return {};
     default:
       return state;
